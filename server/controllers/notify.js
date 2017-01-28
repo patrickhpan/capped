51b500fcbfb7b4fa483c86dda8555e2e378577c7
Patrick Pan <patrickpan@college.harvard.edu>
2017-01-28 16:45:19 -0500

    Handle mongoose connection error

--- server/controllers/notify.js
+++ server/controllers/notify.js
@@ -3,7 +3,7 @@ const { sendMail } = require('../apis/mailgun');
 
 function notify(ytid, email) {
     let message = {
-        subject: 'Your video has been Capped!',
+        subject: `Your video has been Capped! ${new Date()}`,
         html: `Your <a href='https://www.youtube.com/watch?v=${ytid}'>video</a> has been capped!`
     }
     sendMail(email, message);
--- server/scripts/initMongoose.js
+++ server/scripts/initMongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect(process.env.MONGODB);
-
+mongoose.on('error', err => {
+    console.error(`MongoDB connection error: ${err.toString()}`);
+})
 module.exports = mongoose;
