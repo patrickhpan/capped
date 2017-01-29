0d43462b544f5d178aa2dd01db978e9705c437e8
Patrick Pan <patrickpan@college.harvard.edu>
2017-01-29 14:03:36 -0500

    Added instructions for videoInfo

--- server/controllers/videoInfo.js
+++ server/controllers/videoInfo.js
@@ -1,8 +1,14 @@
 function get(ytid) {
+    // Get the analyzed data from a saved database entry of a 
+    // previous analysis of the specified YouTube video
+    // If not present, return null.
     return {};
 }
 
 function set(ytid, data) {
+    // Save the analyzed data into a database entry corresponding
+    // to the YouTube video id (ytid) of the analyzed video
+
     return;
 }
 
