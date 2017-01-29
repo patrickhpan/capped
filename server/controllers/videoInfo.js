c9b4c95ce18589eeb86179b71237133a2c4f9ace
Anna M P <apfoerts@wellesley.edu>
2017-01-29 15:35:13 -0500

    Added controller and model for video info

--- server/controllers/videoInfo.js
+++ server/controllers/videoInfo.js
@@ -1,13 +1,33 @@
+var VideoData = require('../model/VideoData.js');
+
 function get(ytid) {
     // Get the analyzed data from a saved database entry of a 
     // previous analysis of the specified YouTube video
     // If not present, return null.
-    return {};
-}
+    VideoData.findOne({'ytid': ytid}, function (err, video){
+        if (err){
+            console.log('an error occured');
+        } else {
+            if (video) {
+                return video.videoInfo;
+            } else {
+                console.log('video not found');
+                return null;
+            }
+        }
+    });
+};
 
 function set(ytid, data) {
     // Save the analyzed data into a database entry corresponding
     // to the YouTube video id (ytid) of the analyzed video
+    //data can be String version of json file
+    var newVideo = new VideoData({
+        'ytid': ytid,
+        'videoInfo': JSON.stringify(data)
+    });
+
+    newVideo.save();
 
     return;
 }
---
+++ server/model/VideoData.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const videoData = new Schema({
+    //contains either one long String
+    //or an object contain an array containing...
+    ytid:{
+        type: String,
+        required:true,
+    },
+    videoInfo: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('videoData', videoData);
